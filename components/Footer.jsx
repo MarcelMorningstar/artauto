142e4d3a61fabf8121c4e2c456f1c6f635694f36
MarcelMorningstar <rolland1403@gmail.com>
2025-09-09 19:39:01 +0300

    google tag

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 
 export class Footer extends Component {
   render() {
-    return <footer>
-      SIA ARTauto.LV<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M26S6PVG"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    </footer>;
+    return <footer>SIA ARTauto.LV</footer>;
   }
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
-import Script from 'next/script';
 import '../styles/globals.scss';
 import '../styles/navbar.scss';
 import '../styles/footer.scss';
+import { GoogleTagManager } from '@next/third-parties/google'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -11,20 +11,8 @@ function MyApp({ Component, pageProps }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#414141" />
+        <GoogleTagManager gtmId={process.env.GOOGLE_TAG_MANAGER_ID} />
         <link rel="icon" href="/favicon.ico" />
-        <Script 
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];
-              w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-M26S6PVG');`,
-          }}
-        />
         <title>ARTauto</title>
       </Head>
       <Component {...pageProps} />
