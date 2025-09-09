d386063a7e5bdfd571e5d2f7813b015ffcb0ab1d
MarcelMorningstar <rolland1403@gmail.com>
2025-09-09 11:29:57 +0300

    google tag

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 
 export class Footer extends Component {
   render() {
-    return <footer>SIA ARTauto.LV</footer>;
+    return <footer>
+      SIA ARTauto.LV<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M26S6PVG"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    </footer>;
   }
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import '../styles/globals.scss';
 import '../styles/navbar.scss';
 import '../styles/footer.scss';
@@ -11,6 +12,19 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#414141" />
         <link rel="icon" href="/favicon.ico" />
+        <Script 
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M26S6PVG');`,
+          }}
+        />
         <title>ARTauto</title>
       </Head>
       <Component {...pageProps} />
