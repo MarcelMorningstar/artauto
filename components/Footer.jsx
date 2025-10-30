8b91660def6a51ce1ab3297eb35e2a4e3071309e
MarcelMorningstar <rolland1403@gmail.com>
2025-10-30 21:01:12 +0200

    logo

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import Image from 'next/image';
 
 export class Footer extends Component {
   render() {
-    return <footer>SIA ARTauto.LV</footer>;
+    return (
+      <footer>
+        SIA ARTauto.LV
+        <Image id="logo" src="/img/logo.png" alt="ARTauto Logo" width={120} height={120} />
+      </footer>
+    );
   }
 }
 
